f2c13a7a14a0fff307055c64525c6210a9de8b6b
Victor L. <victor.bcn.17@gmail.com>
2025-02-27 20:35:06 +0100

    Update 1.2 Initial data wrangling

--- practice.r
+++ practice.r
@@ -1,3 +1,4 @@
+# Library Import
 library("tidyverse")
 library("ggthemes")
 
@@ -10,11 +11,24 @@ df5 <- select(read.csv("./data/qkh.csv"), part_id, gender, age, lang_ca, lang_sp
 df6 <- select(read.csv("./data/y8r.csv"), part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
 
 # Data Cleaning
-df <- reduce(list(df1, df2, df3, df4, df5, df6), rbind)
-df_clean <- df[order(df$part_id), ] %>%
+df_raw <- reduce(list(df1, df2, df3, df4, df5, df6), rbind)
+
+df_clean <- df_raw[order(df_raw$part_id), ] %>%
     filter(task == "item" | task == "question") %>%
     mutate(across(c(lang_ca, lang_en, lang_sp, correct_answer, answer), as.logical))
 
 df_clean$lang_ca[is.na(df_clean$lang_ca)] <- FALSE
 df_clean$lang_sp[is.na(df_clean$lang_sp)] <- FALSE
 df_clean$lang_en[is.na(df_clean$lang_en)] <- FALSE
+
+df_rt <- filter(select(df_clean, part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt), task == "item")
+
+df_acc <- filter(select(df_clean, part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, correct_answer, answer), task == "question") %>%
+    mutate(correct = ifelse(correct_answer == answer, TRUE, FALSE))
+
+# Data Analysis
+rt_lm <- lm(data = df_rt, rt ~ clause_type + number)
+summary(rt_lm)
+
+acc_lm <- lm(data = df_acc, correct ~ clause_type + number)
+summary(acc_lm)
