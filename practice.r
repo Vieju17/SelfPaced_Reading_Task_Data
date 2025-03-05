750ac8d4993a78699646b5af6e48b5e45ce60dd6
Victor L. <victor.bcn.17@gmail.com>
2025-03-05 19:27:43 +0100

    Update 1.5 Encoded categorical variables

--- practice.r
+++ practice.r
@@ -3,44 +3,57 @@ library(tidyverse)
 library(ggthemes)
 
 # Data Import
-df1 <- select(read.csv("./data/7co.csv"), part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
-df2 <- select(read.csv("./data/ayb.csv"), part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
-df3 <- select(read.csv("./data/ggn.csv"), part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
-df4 <- select(read.csv("./data/h1n.csv"), part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
-df5 <- select(read.csv("./data/qkh.csv"), part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
-df6 <- select(read.csv("./data/y8r.csv"), part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
+sample <- read.csv("./data/0x0.csv")
+head(sample)
 
-# Data Cleaning
-df_raw <- reduce(list(df1, df2, df3, df4, df5, df6), rbind)
+df1 <- select(read.csv("./data/0x0.csv"), part_id, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
+df2 <- select(read.csv("./data/bzm.csv"), part_id, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
+df3 <- select(read.csv("./data/dj9.csv"), part_id, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
+df4 <- select(read.csv("./data/r5t.csv"), part_id, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
+df5 <- select(read.csv("./data/s62.csv"), part_id, task, item_type, clause_type, number, segment, rt, correct_answer, answer)
+
+df_raw <- reduce(list(df1, df2, df3, df4, df5), rbind)
 summary(df_raw)
 
-df_clean <- df_raw[order(df_raw$part_id), ] %>%
-    filter(task == "item" | task == "question") %>%
-    mutate(across(c(lang_ca, lang_en, lang_sp, correct_answer, answer), as.logical)) %>%
-    mutate(rt = as.numeric(rt))
+# Data Cleaning
+df_clean <- df_raw[order(df_raw$part_id), ] %>% # Order by participant ID
+    filter((task == "item" | task == "question") & segment != "s0" & item_type != "pr_item") %>% # Remove fixations and s0 segments
+    mutate(rt = as.numeric(rt)) %>% # Making RTs a numeric value
+    mutate(clause_type = ifelse(clause_type == "sr", 1, 2)) %>% # Recoding clause type
+    mutate(number = ifelse(number == "ss", 1,
+        ifelse(number == "pp", 2,
+            ifelse(number == "sp", 3, 4)
+        )
+    )) %>% # Making number a numeric value
+    mutate(segment = ifelse(segment == "s1", 1,
+        ifelse(segment == "s2", 2,
+            ifelse(segment == "s3", 3,
+                ifelse(segment == "s4", 4, 5)
+            )
+        )
+    )) # Making number a numeric value
 summary(df_clean)
 
-df_clean$lang_ca[is.na(df_clean$lang_ca)] <- FALSE
-df_clean$lang_sp[is.na(df_clean$lang_sp)] <- FALSE
-df_clean$lang_en[is.na(df_clean$lang_en)] <- FALSE
-
+# Outlier Detection Variables
 Q1 <- quantile(df_clean$rt, 0.25) # nolint
 Q3 <- quantile(df_clean$rt, 0.75) # nolint
 IQR <- Q3 - Q1 # nolint
 inner_bound <- Q1 - 1.5 * IQR
 upper_bound <- Q3 + 1.5 * IQR
 
-df_rt <- filter(select(df_clean, part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, segment, rt), task == "item" & item_type == "exp_item") %>%
+# Data Frame for Performance
+df_rt <- filter(select(df_clean, part_id, task, item_type, clause_type, number, segment, rt), task == "item" & item_type == "exp_item") %>%
     filter(rt > inner_bound & rt < upper_bound) %>%
     mutate(rt = log(rt, base = 2))
 summary(df_rt)
 
-df_acc <- filter(select(df_clean, part_id, gender, age, lang_ca, lang_sp, lang_en, task, item_type, clause_type, number, correct_answer, answer), task == "question") %>%
-    mutate(correct = ifelse(correct_answer == answer, TRUE, FALSE))
+# Data Frame for Accuracy
+df_acc <- filter(select(df_clean, part_id, task, item_type, clause_type, number, correct_answer, answer), task == "question") %>%
+    mutate(correct = ifelse(correct_answer == answer, 0, 1))
 summary(df_acc)
 
 # Data Analysis
-lm_rt <- lm(data = df_rt, rt ~ clause_type + number)
+lm_rt <- lm(data = df_rt, rt ~ clause_type + number + segment)
 summary(lm_rt)
 
 lm_acc <- lm(data = df_acc, correct ~ clause_type + number)
@@ -51,8 +64,8 @@ plot_rt <- df_rt %>%
     ggplot(aes(
         x = clause_type,
         y = rt,
-        color = clause_type,
-        fill = clause_type
+        color = number,
+        fill = number
     )) +
     geom_boxplot(
         notch = TRUE,
